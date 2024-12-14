f9fdb4e97c6c910c9a352a199b1cbef6c2468b92
Digna Patel <dignapatel0806@gmail.com>
2024-12-13 22:43:46 -0500

    Cumulative 3 added new

--- Cumulative/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Cumulative.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
