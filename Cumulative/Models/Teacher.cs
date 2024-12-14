49b0015229c2abde59a243e40c5dcd87e302f59c
Digna Patel <dignapatel0806@gmail.com>
2024-12-13 22:38:05 -0500

    Cumulative 3 added

---
+++ Cumulative/Models/Teacher.cs
@@ -0,0 +1,25 @@
+﻿namespace Cumulative.Models
+{
+    public class Teacher
+    {
+        //Represents Teacher
+
+        //Teacher id
+        public int TeacherId { get; set; }
+
+        //Teacher first name
+        public string TeacherFName { get; set; }
+
+        //Teacher last name
+        public string TeacherLName { get; set; }
+
+        // Teacher employee number
+        public string EmployeeNumber { get; set; }
+
+        // Teacher hiredate
+        public DateTime HireDate { get; set; }
+
+        //Teacher salary
+        public decimal Salary { get; set; }
+    }
+}
